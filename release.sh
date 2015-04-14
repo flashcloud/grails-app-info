96508668ca2f4187216a28fdbe7bb5ae7b002c19
Christopher Legan <christopherl@criticalmass.com>
2015-04-14 13:25:53 -0500

    zip packaging

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 rm -rf target/release
 mkdir target/release
 cd target/release
-git clone git@github.com:burtbeckwith/grails-app-info.git
+git clone git@github.com:ceekayel/grails-app-info.git
 cd grails-app-info
 grails clean
 grails compile
